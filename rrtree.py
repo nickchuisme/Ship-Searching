44e02594e35001626b510be29954db92ea8ac04b
Nick Chu <37206164+chuchum@users.noreply.github.com>
2018-05-25 16:23:25 +0800

    Update rrtree.py

--- rrtree.py
+++ rrtree.py
@@ -27,15 +27,13 @@ class tree():
 			self.MMSI.append(mmsi)
 			self.shapes[num] = shape(f['geometry']) 
 			self.properties[num] = f['properties']
-			#print(num,properties[num]['End_time'])
 			self.End_timeList.append(self.properties[num]['End_time'])
 			self.Start_timeList.append(self.properties[num]['Start_time'])
 		#insert index
 		for number,line in enumerate(self.shapes.items()):
-			#print(number)
 			self.idx.insert(number, line[1].bounds)
 		
-# 找重疊(第一次找線的bounding box與輸入之bounding box 第二次找線和輸入之bounding box)
+	# 找重疊(第一次找線的bounding box與輸入之bounding box 第二次找線和輸入之bounding box)
 	def FindOverlap(self,minX,minY,maxX,maxY):
 		#get index
 		callindex = tree.createindex()
@@ -43,9 +41,7 @@ class tree():
 		boundingBoxList = [Point(minX,minY),Point(minX,maxY),Point(maxX,maxY),Point(maxX,minY)]
 		poly = geometry.Polygon([[p.x, p.y] for p in boundingBoxList])
 		print('BoundingBox: %s'%(poly)) 
-		#select the intersect target
-		#print('mmsi= %s')%(MMSI)
-		
+	
 		##########
 		
 		spatialref = osr.SpatialReference()  # Set the spatial ref.
@@ -73,21 +69,16 @@ class tree():
 		dstlayer.CreateField(fielddef)
 		
 		############
-		
-		for x,linestring in zip(sorted(list(self.idx.intersection(input_boundingBox))),self.shapes.items()):
-			#time.sleep(0.01)
+		for x in sorted(list(self.idx.intersection(input_boundingBox))):
+			#print(x)
+			linestring=self.shapes[x]
 			
-			if (poly.intersects(linestring[1]) == True and tree.checktime(self.Start_timeList[x],self.End_timeList[x],date_format,x) == True):
+			if (poly.intersects(linestring) == True and \
+				tree.checktime(self.Start_timeList[x],self.End_timeList[x],date_format,x) == True):
+				
 				print('%s (%s) is selected...'%(x,self.MMSI[x]))
 				self.SelectedList.append(int(x))
-				#print (x)
-				#print(linestring[1])
-				#print('start time:%s')%(Start_timeList[x])
-				#print('end time:%s')%(End_timeList[x])
-				#print('dstlayer in overlap = %s')%(dstlayer)
-				#line = ogr.Geometry(ogr.wkbLineString)
-				line= ogr.CreateGeometryFromWkt(str(linestring[1]))
-				#print('line = %s')%(line)
+				line= ogr.CreateGeometryFromWkt(str(linestring))
 				feature = ogr.Feature(dstlayer.GetLayerDefn())
 				feature.SetGeometry(line)
 				feature.SetField("MMSI", self.MMSI[x])
@@ -97,10 +88,10 @@ class tree():
 				dstlayer.CreateFeature(feature)
 			else:
 				print('%s (%s) is not used...'%(x,self.MMSI[x]))
+		
 		print('SelectedList: %s'%(self.SelectedList))
 		print('Length of SelectedList: %s'%(len(self.SelectedList)))
 		
-		#feature = ogr.Feature(dstlayer.GetLayerDefn())
 		polygon = ogr.CreateGeometryFromWkt(str(poly))
 		feature.SetGeometry(polygon)
 		feature.SetField("MMSI", 'boundingbox')
@@ -112,10 +103,6 @@ class tree():
 	# 檢查時間
 	def checktime(self,From_time,To_time,date_format,id):
 		global LeftTimeBounding, RightTimeBounding
-		#print('LeftTimeBounding = %s')%(LeftTimeBounding)
-		#print('RightTimeBounding = %s')%(RightTimeBounding)
-		#print('From_time = %s')%(From_time)
-		#print('To_time = %s')%(To_time)
 		if(date_format == 1):
 			date_format = '%Y-%m-%d %H:%M:%S'
 		elif(date_format == 2):
@@ -128,14 +115,11 @@ class tree():
 		
 		#select time overlap
 		if((From_time>RightTimeBounding) is False or (To_time<LeftTimeBounding) is False):
-			#print('%s is selected...')%(id)
 			return True
 
 
 if __name__ == "__main__":
 	tree = tree()
-	#LeftTimeBounding = '2000/01/01 00:00:00'
-	#RightTimeBounding = '2020/01/01 00:00:00'
 	LeftTimeBounding = raw_input("StartingTime: ")
 	RightTimeBounding = raw_input("EndTime: ")
 	date_format = int(raw_input("Timeformat[input 1.-, 2./]: "))
